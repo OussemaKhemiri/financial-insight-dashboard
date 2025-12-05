1da34e218aeb84accdc3a03974979a99f65f8493
OussemaKhemiri <khemiri.oussema@esprit.tn>
2025-12-05 01:17:11 +0100

    fix

--- src/components/forex/CalendarEmbed.tsx
+++ src/components/forex/CalendarEmbed.tsx
@@ -7,12 +7,12 @@ export default function CalendarEmbed() {
             </div>
 
             {/* Iframe Container */}
-            <div className="flex-2 w-full relative">
+            {/* FIX 1: Changed 'flex-2' to 'flex-1' so it actually fills the space */}
+            <div className="flex-1 w-full relative">
                 <iframe
                     src="https://sslecal2.investing.com?ecoDayBackground=%23162633&innerBorderColor=%237800b8&borderColor=%23000000&ecoDayFontColor=%23ffffff&columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&features=datepicker,timezone,timeselector,filters&countries=25,32,6,37,72,22,17,39,14,10,35,43,56,36,110,11,26,12,4,5&calType=week&timeZone=16&lang=1"
                     className="absolute inset-0 w-full h-full border-0"
                     title="Investing.com Economic Calendar"
-                    allowtransparency="true"
                 />
             </div>
 
